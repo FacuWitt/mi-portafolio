ca34ddb3426d5064ca81db298ccc7cb02d9eb8a8
FacuWitt <facu.witt@gmail.com>
2026-03-31 18:59:39 -0300

    FIX vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  base: '/nombre-de-tu-repositorio/', // <--- Reemplaza con el nombre exacto de tu repo en GitHub
+})
