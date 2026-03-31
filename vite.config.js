f95b4558133efd2c79fdd96255d769dceb1ff0cd
FacuWitt <facu.witt@gmail.com>
2026-03-31 19:01:28 -0300

    FIX vite.config.js nombre de repositorio

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nombre-de-tu-repositorio/', // <--- Reemplaza con el nombre exacto de tu repo en GitHub
+  base: '/mi-portafolio/', 
 })
